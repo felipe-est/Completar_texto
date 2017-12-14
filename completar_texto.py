3482e6fee15b7a8e3eedfe1f784d4f9750adacea
felipe-est <fellipe.alves@gmail.com>
2017-12-14 00:30:12 -0200

    Ajustes após revisão do código. Inclusao de mais funcoes e melhoria da estrutura

--- completar_texto.py
+++ completar_texto.py
@@ -23,6 +23,9 @@ texto_dificil = """A frase "Eu ___1___ gente morta." ficou famosa no filme "O se
 Já a frase "A primeira ___6___ no ___7___ da ___8___ é não falar sobre o ___7___ da ___8___" foi dita no filme "O ___7___ da ___8___" pelo personagem ___9___, intepretado por ___10___ ___11___.
 """
 
+# Juntando textos em uma lista
+textos = [texto_facil, texto_medio, texto_dificil]
+
 # Respostas
 # Facil: 1-Vejo, 2-sentido, 3-regra, 4-clube, 5-luta
 # Medio: 1-Vejo, 2-sentido, 3-Cole, 4-regra, 5-clube, 6-luta, 7-Tyler
@@ -32,72 +35,128 @@ resp_facil = ["vejo","sentido","regra","clube","luta"]
 resp_medio = ["vejo","sentido","Cole","regra","clube","luta","Tyler"]
 resp_dificil = ["vejo","sentido","Cole","Haley","Joel","regra","clube","luta","Tyler","Brad","Pitt"]
 
-#print texto_facil
-#print texto_medio
-#print texto_dificil
+# Juntando respostas em uma lista
+respostas = [resp_facil,resp_medio,resp_dificil]
 
-# Definindo texto inicial do quiz e solicitando o nivel ao jogador
+# Definindo os niveis
+niveis = ["fácil","médio","difícil"]
 
-print """Bem vindo ao Quiz sobre filmes.
-Você consegue de completar as frases corretamente?"""
+# Funcao de definicao do texto dos niveis para informar o usuario as opcoes disponiveis
+# Inputs:
+#   - niveis: lista de niveis disponiveis para o jogo
+# Outputs:
+#   - niveis_jogo: texto contendo a lista de niveis separada por virgula
+def texto_niveis(niveis):
+    niveis_jogo = ""
+    for qtd_niveis in niveis:
+        niveis_jogo += qtd_niveis + ", "
+    if niveis_jogo[-2] == ",":
+        niveis_jogo = niveis_jogo[:-2] + "."
+    return niveis_jogo
 
-nivel = raw_input("Os níveis são facil, medio ou dificil. Escolha um nível: ")
+# Solicitando o nivel ao jogador
+# Inputs:
+#   - niveis: lista de niveis definidos para o quiz
+# Outputs:
+#   - nivel_usuario: texto do nivel escolhido pelo usuario
+def nivel_usuario(niveis):
+    # Pergunta ao usuario qual nivel do jogo
+    nivel_usuario = raw_input("Os níveis disponíveis são: " + texto_niveis(niveis) + " Escolha um deles: ")
+    # Teste para garantir que o jogador escolheu um nivel valido
+    while nivel_usuario not in niveis:
+        nivel_usuario = raw_input("Os níveis disponíveis são: " + texto_niveis(niveis) + " Escolha um deles: ")
+    return nivel_usuario
 
-# Teste para garantir que o jogador escolheu um nivel valido
-if nivel == "facil":
-    print texto_facil
-elif nivel == "medio":
-    print texto_medio
-elif nivel == "dificil":
-    print texto_dificil
-else:
-    print "Escolha entre os niveis facil, medio ou dificil."
+# Recebe o nível escolhido pelo usuario em texto e retorna o nivel escolhido em numero
+# Inputs:
+#   - nivel_usuario: niveis escolhido pelo usuario em texto
+# Outputs:
+#   - nivel_usuario_num: nivel escolhido pelo usuario em numero
+def nivel_usuario_num(nivel_usuario):
+    # Loop para verificar a posicao do nivel escolhido dentro da lista e retorno do nivel em numero
+    n_nivel = 0
+    nivel_usuario_num = 0
+    while n_nivel < len(niveis):
+        if nivel_usuario == niveis[n_nivel]:
+            nivel_usuario_num = n_nivel
+        n_nivel += 1
+    return nivel_usuario_num
+
+
+# Definindo funcao que verifica se resposta está correta
+# Inputs:
+#   - palavra: texto da lacuna correspondente à tentativa do usuario
+#   - texto: texto do quiz com a lacuna ainda nao preenchida
+#   - resposta: lista das respostas corretas para verificar se o usuario acertou
+#   - lacunas: numero referente à lacuna que o usuario está respondendo
+# Outputs
+#   - texto: texto com a lacuna preenchida caso o usuario acerte
+# Caso o usuario nao acerte, a funcao perguntara a resposta novamente
+
+def verifica (palavra, texto, resposta, lacunas):
+    if palavra in texto:
+        # Solicitando resposta do usuario
+        troca = raw_input("A palavra " + palavra + "é: ")
+        # Loop para solicitar nova resposta em caso de erro
+        while troca not in resposta[lacunas-1]: # -1 porque o numero de lacunas começa em 1 e o python em 0
+            print """
+Tente novamente, você errou :/
+            """
+            print texto
+            troca = raw_input("A palavra " + palavra + "é: ")
+        # Em caso de acerto, substitui a lacuna, printa o texto com a lacuna preenchida
+        texto = texto.replace(palavra, troca)
+        print """
+Você acertou!! :)
+"""
+        print texto
+    return texto
+
 
 # Definindo funcao do jogo
+# Inputs:
+#   - niveis: lista de niveis disponiveis
+#   - respostas: lista de respostas disponiveis
+#   - textos: lista de textos com lacunas disponiveis
+# Mostrará o texto do nivel escolhido e perguntará a resposta da primeira lacuna
+# Em caso de acerto, passa para a proxima. Em caso de erro, pede nova tentativa ao usuario
+# Output:
+#   - Mensagem de parabens por completar o jogo
 
-def subst(nivel):
-    if nivel == "facil":
-        resp=resp_facil
-        texto=texto_facil
-        texto_split=texto_facil.split()
+def jogo(niveis, respostas, textos):
 
-    if nivel == "medio":
-            resp=resp_medio
-            texto=texto_medio
-            texto_split=texto_medio.split()
+    print """
+Bem vindo ao Quiz sobre filmes.
 
-    if nivel == "dificil":
-            resp=resp_dificil
-            texto=texto_dificil
-            texto_split=texto_dificil.split()
+Você consegue de completar as frases corretamente?
+    """
+
+    # Definicao de nivel pelo usuario
+    nivel_escolhido = nivel_usuario_num(nivel_usuario(niveis))
+
+    # Definicao dos textos de quiz e resposta do nivel selecionado
+    resp = respostas[nivel_escolhido]
+    texto = textos[nivel_escolhido]
+
+    #Print do texto escolhido pelo usuario
+    print """
+O jogo começou!
+
+Tente completar as frases abaixo do nível """ + niveis[nivel_escolhido] + """!
+    """
+    print texto
+
     # Iniciando contador das lacunas. Esse contador irá até o total de lacunas do nível.
-    i = 1
+    cont_lacunas = 1
     # Iniciando loop while para perguntar a resposta de cada lacuna
-    while i <= len(resp):
+    while cont_lacunas <= len(resp):
         # Definindo o texto de cada lacuna iterativamente
-        palavra = "___"+str(i)+"___"
-        # Iniciando contador para cada palavra do texto a ser preenchido
+        lacuna = "___"+str(cont_lacunas)+"___"
         # Se encontrar uma lacuna, o programa pede uma resposta e só continua se o jogador acertar
-        p = 0
-        # Iniciando loop para cada palavra do texto
-        while p < len(texto_split):
-            # Se a lacuna estiver no texto, solicita respota do usuário
-            if palavra in texto_split[p]:
-                # Solicitando resposta do usuario
-                troca = raw_input("A palavra " + palavra + "é: ")
-                # Loop para solicitar nova resposta em caso de erro
-                while troca not in resp[i-1]:
-                    print "Tente novamente"
-                    print texto
-                    troca = raw_input("A palavra " + palavra + "é: ")
-                # Em caso de acerto, substitui a lacuna, printa o texto com a lacuna preenchida
-                texto = texto.replace(palavra, troca)
-                texto_split=texto.split()
-                print "Você acertou"
-                print texto
-            p += 1
-        i += 1
+        # Se a lacuna estiver no texto, solicita respota do usuário
+        texto = verifica(lacuna,texto,resp,cont_lacunas)
+        cont_lacunas += 1
     # Ao acertar tudo, o jogo acaba
     return "Parabéns! Você ganhou!"
 
-print subst(nivel)
+print jogo(niveis, respostas, textos)
